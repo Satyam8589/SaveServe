9692ce7161c030f1ecc6be6b67923ea94273a028
Satyam8589 <satyamkumarsingh9898@gmail.com>
2025-08-09 22:02:16 +0530

    startar template is created

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Welcome to My Developer!</h1>
+      <p className="mt-4 text-lg">This is a simple page using Next.js.</p>
+      </main>
+  );
+}
